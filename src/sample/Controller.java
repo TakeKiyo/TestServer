1728b56c5d19759bc0f87b745b29a9f69b3166cf
Takeda Kiyoshi <Oldmaid138@gmail.com>
2019-09-03 13:20:36 +0900

    存在しているスレッドを破棄して新しくスレッドを作成

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextArea;
 
 import java.net.*;
 import java.io.*;
+import java.util.Objects;
 
 public class Controller {
     public Button start;
@@ -15,13 +16,24 @@ public class Controller {
     public void Clicked(ActionEvent event) {
         String result ="接続されました";
         resultLabel.setText(result);
+        MyThread thread = null;
+        Socket socket = null;
         try {
             ServerSocket ServerSocket = new ServerSocket(5000);
             while(true) {
-                Socket socket = ServerSocket.accept();
-                MyThread thread= new MyThread(socket);
-                thread.start();
 
+                System.out.println("null?:" + Objects.nonNull(thread));
+
+
+
+
+                if (Objects.nonNull(thread)){
+                    thread.destroyThread();
+                }
+                System.out.println("null?:" + Objects.nonNull(thread));
+                socket = ServerSocket.accept();
+                thread= new MyThread(socket);
+                thread.start();
             }
         }catch(IOException e){
             System.out.println(e);
@@ -34,31 +46,41 @@ public class Controller {
 
     class MyThread extends Thread {
         private Socket socket;
+        private boolean isActive;
 
         public MyThread(Socket socket){
             this.socket = socket;
+            this.isActive = true;
+        }
+
+        public void destroyThread(){
+            this.isActive = false;
         }
 
         public void run() {
-            try {
-                //データ受信
-                InputStream cInput = socket.getInputStream();
-                InputStreamReader cInputReader = new InputStreamReader(cInput);
-                BufferedReader cBreader = new BufferedReader(cInputReader);
-                String sData = cBreader.readLine();
-                Output.setText(sData + "\n"+ Output.getText() );
-                System.out.println("受信データ:" + sData);
+            while(this.isActive) {
+                try {
+                    //データ受信
+                    InputStream cInput = socket.getInputStream();
+                    InputStreamReader cInputReader = new InputStreamReader(cInput);
+                    BufferedReader cBreader = new BufferedReader(cInputReader);
+                    String sData = cBreader.readLine();
+                    Output.setText(sData + "\n" + Output.getText());
+                    System.out.println("受信データ:" + sData);
 
-                //データ(受信文+送った)を送信
-                OutputStream cOut = socket.getOutputStream();
-                OutputStreamWriter cOutwriter = new OutputStreamWriter(cOut);
-                cOutwriter.write(sData + "送った");
-                cOutwriter.flush();
-                socket.close();
-                System.out.println(Output.getText());
-            }catch(IOException e){
-                System.out.println(e);
+                    //データ(受信文+送った)を送信
+                    OutputStream cOut = socket.getOutputStream();
+                    OutputStreamWriter cOutwriter = new OutputStreamWriter(cOut);
+                    cOutwriter.write(sData + "送った");
+                    cOutwriter.flush();
+                    socket.close();
+                    System.out.println(Output.getText());
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
             }
+
+
         }
 
     }

039b571ba1d558b016924352d85d258ebcef4de9
Takeda Kiyoshi <Oldmaid138@gmail.com>
2019-09-12 16:36:48 +0900

    make_bccを実装

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,9 @@ import java.util.Objects;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Controller {
     public Button start;
@@ -21,15 +24,51 @@ public class Controller {
     public TextArea Output;
     public Statement statement = null;
     public Connection connection = null;
+    public List<String> bcc_byte = new ArrayList<String>();
+    public String[] bcc_string = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
     public void Clicked(ActionEvent event) {
         String txt ="接続されました";
         resultLabel.setText(txt);
         StartThread StartThread = new StartThread();
         StartThread.start();
+        bcc_byte.add("0000");
+        bcc_byte.add("0001");
+        bcc_byte.add("0010");
+        bcc_byte.add("0011");
+        bcc_byte.add("0100");
+        bcc_byte.add("0101");
+        bcc_byte.add("0110");
+        bcc_byte.add("0111");
+        bcc_byte.add("1000");
+        bcc_byte.add("1001");
+        bcc_byte.add("1010");
+        bcc_byte.add("1011");
+        bcc_byte.add("1100");
+        bcc_byte.add("1101");
+        bcc_byte.add("1110");
+        bcc_byte.add("1111");
     }
 
     public void Finish(ActionEvent event) {
         System.exit(0);
+    }
+    public String make_bcc(String data){
+        byte[] data_byte = data.getBytes();
+        byte res = (byte) (data_byte[0] ^ data_byte[1]);
+        for (int i=2;i<data_byte.length;i++){
+            res = (byte)(res ^ data_byte[i]);
+        }
+
+        String str = Integer.toBinaryString(res);
+        str = String.format("%8s", str).replace(' ', '0');
+        String first = str.substring(0,4);
+        String second = str.substring(4,8);
+        int first_idx = bcc_byte.indexOf(first);
+        int second_idx = bcc_byte.indexOf(second);
+        first = bcc_string[first_idx];
+        second = bcc_string[second_idx];
+        return first+second;
+
     }
 
 
@@ -135,7 +174,8 @@ public class Controller {
                     String second = new String(receiveBuf);
                     String str = first + second;
                     String receivedBCC = new String(receiveBCC);
-                    System.out.println(receivedBCC);
+                    System.out.println("受け取ったbcc:"+receivedBCC);
+                    System.out.println("calculatd bcc:"+ make_bcc(str));
                     System.out.println("got:"+str);
                     String txt = Output.getText();
                     Output.setText(txt + "\n" + str);
